fc1dacc5e7525c3a349b373d284d732eb945b8e7
Arman Yerkesh <a.yerkesh@gmail.com>
2022-12-02 10:19:56 +0200

    If destination fetch failed - do 2 more tries. If destination cannot be reached - abort the game

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,14 +1,31 @@
 "use strict";
 
-// Fetch final destination
-fetch('dest')
-    .then((response) => {
-        return response.json();
-    })
-    .then((destination) => {
-        console.log(typeof destination);
-        console.dir(destination);
-    })
-    .catch((error) => {
-        console.error('Failed to fetch destination. Error: ' + error.message);
-    });
+async function main() {
+    // Fetch final destination
+    const dest = await fetchDest();
+
+    console.log('Destination:');
+    console.dir(dest);
+
+    // If destination fetch failed - make 2 more tries. If both of them fail - abort the game
+    let tries = 0;
+    while(!dest && tries<2) {
+        dest = await fetchDest();
+        tries++;
+    }
+    if (!dest) { return; }
+}
+
+main();
+
+// FUNCTIONS
+function fetchDest() {
+    const promise = fetch('dest')
+        .then((response) => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Failed to fetch destination. Error: ' + error.message);
+        });
+    return promise;
+}
