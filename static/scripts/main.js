5945c556f0d45f33f6de4c681e88c7fb1ddcbf04
Arman Yerkesh <a.yerkesh@gmail.com>
2022-12-02 09:40:45 +0200

    Fetch destination to front-end

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log('Hello from JavaScript');
+// Fetch final destination
+fetch('dest')
+    .then((response) => {
+        return response.json();
+    })
+    .then((destination) => {
+        console.log(typeof destination);
+        console.dir(destination);
+    })
+    .catch((error) => {
+        console.error('Failed to fetch destination. Error: ' + error.message);
+    });
