821f195347a8504a185f681fa9108adb8fae6822
Arman Yerkesh <a.yerkesh@gmail.com>
2022-12-02 10:46:10 +0200

    Define JS function to fetch resource in several tries

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,30 +2,45 @@
 
 async function main() {
     // Fetch final destination
-    const dest = await fetchDest();
+    const dest = await fetchTimes('dest', 3, 'destination');
+    // If after 3 tries dest is undefined - abort the game
+    if (!dest) { return; }
 
     console.log('Destination:');
     console.dir(dest);
 
-    // If destination fetch failed - make 2 more tries. If both of them fail - abort the game
-    let tries = 0;
-    while(!dest && tries<2) {
-        dest = await fetchDest();
-        tries++;
-    }
-    if (!dest) { return; }
+    // Fetch starting location, list of available airports, distance to destination,
+    // current turn num, total km travelled and total co2 emissed
+    const currentData = await fetchTimes('current', 3, 'current data');
+    if (!currentData || Object.keys(currentData).length === 0) { return; }
+
+    console.log('Current data:');
+    console.dir(currentData);
 }
 
 main();
 
 // FUNCTIONS
-function fetchDest() {
-    const promise = fetch('dest')
+function fetchFrom(url, resource) {
+    const promise = fetch(String(url))
         .then((response) => {
             return response.json();
         })
         .catch((error) => {
-            console.error('Failed to fetch destination. Error: ' + error.message);
+            console.error(`Failed to fetch ${String(resource)}. Error: ${error.message}`);
         });
     return promise;
+}
+
+// Make several tries to fetch a resourse
+async function fetchTimes(url, times=1, resource) {
+    let res = await fetchFrom(url, resource);
+    let tries = 1;
+
+    while(!res && tries<Number(times)) {
+        res = await fetchFrom(url, resource);
+        tries++;
+    }
+
+    return res;
 }
