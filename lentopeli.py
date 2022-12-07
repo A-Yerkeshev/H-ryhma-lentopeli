211ab6c6c7778323e2c4b0093b6ab4e56c5a1f3b
Arman Yerkesh <a.yerkesh@gmail.com>
2022-12-07 11:51:16 +0200

    Calculate co2 emission

--- lentopeli.py
+++ lentopeli.py
@@ -1,4 +1,4 @@
-import mysql.connector, os, math, time, json, jsonpickle
+import mysql.connector, os, math, time, json, jsonpickle, requests
 from flask import Flask, send_file
 from geopy import distance
 from dotenv import load_dotenv
@@ -60,6 +60,14 @@ def send_current():
     airports.clear()
     airports = fetch_available_airports(curr.lat, curr.long, curr.type)
 
+    # Add direction and distance
+    for airport in airports:
+        add_direction(airport)
+        airport.distance = round(get_distance(curr.lat, curr.long, airport.lat, airport.long), 2)
+
+    # Calculate CO2 emission
+    add_co2(airports)
+
     currdata = {'current': curr, 'airports': airports, 'dist': dist,
                 'turn': turns_total, 'total_km': km_total, 'total_co2': co2_total}
     return jsonpickle.encode(currdata)
@@ -67,7 +75,7 @@ def send_current():
 # FUNCTIONS
 def generate_random_location():
     sql = "SELECT ident, airport.name as airport_name," \
-          "country.name as country_name, type, latitude_deg, longitude_deg " \
+          "country.name as country_name, type, latitude_deg, longitude_deg, iata_code " \
           "FROM airport, country WHERE NOT type='closed' " \
           "and airport.iso_country = country.iso_country" \
           " ORDER BY RAND() LIMIT 1;"
@@ -75,20 +83,20 @@ def generate_random_location():
     result = cursor.fetchall()
 
     airport = Airport(result[0][0], result[0][1], result[0][2],
-                      result[0][3], result[0][4], result[0][5])
+                      result[0][3], result[0][4], result[0][5], result[0][6])
     return airport
 
-def tuple_to_dict(tuple):
-    ident, airport_name, country_name, type, lat, long = tuple
+# def tuple_to_dict(tuple):
+#     ident, airport_name, country_name, type, lat, long = tuple
 
-    return {
-        "ident": ident,
-        "airport_name": airport_name,
-        "country_name": country_name,
-        "type": type,
-        "lat": lat,
-        "long": long,
-    }
+#     return {
+#         "ident": ident,
+#         "airport_name": airport_name,
+#         "country_name": country_name,
+#         "type": type,
+#         "lat": lat,
+#         "long": long,
+#     }
 
 def get_distance(curr_lat, curr_long, dest_lat, dest_long):
     distance_result = distance.distance([curr_lat, curr_long],
@@ -104,7 +112,7 @@ def fetch_available_airports(curr_lat, curr_long, type):
 
     # Select all airports within the reach of current location, based on airport type
     # Distance = 3963.0 * arccos[(sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2 – long1)] * 1.609344
-    sql = f"""SELECT ident, airport.name, country.name, type, latitude_deg, longitude_deg FROM airport, country
+    sql = f"""SELECT ident, airport.name, country.name, type, latitude_deg, longitude_deg, iata_code FROM airport, country
     WHERE 3963.0 * acos((sin(RADIANS({curr_lat})) * sin(RADIANS(latitude_deg))) +
     cos(RADIANS({curr_lat})) * cos(RADIANS(latitude_deg)) *
     cos(RADIANS(longitude_deg) - RADIANS({curr_long}))) * 1.609344 <= {radius_km}
@@ -117,18 +125,16 @@ def fetch_available_airports(curr_lat, curr_long, type):
 
     result = []
 
-    # Create airport object from entries of db_results and add direction and distance
+    # Create airport object from entries of db_results
     for entry in db_result:
-        airport = Airport(entry[0], entry[1], entry[2], entry[3], entry[4], entry[5])
-        
-        add_direction(airport)
-        airport.distance = round(get_distance(curr.lat, curr.long, airport.lat, airport.long), 2)
-
+        airport = Airport(entry[0], entry[1], entry[2], entry[3], entry[4], entry[5], entry[6])
         result.append(airport)
 
     return result
 
 def add_direction(airport):
+    global curr
+
     direction_names = ['North', 'North-East', 'East', 'South-East', 'South', 'South-West', 'West', 'North-West']
 
     # Calculate in which direction airport is located
@@ -144,17 +150,51 @@ def add_direction(airport):
     i = round(((bearing + 180)/45)+4)%8
     airport.direction = direction_names[i]
 
+def add_co2(airports):
+    global curr
+
+    computable_flights = []
+    airport_indexes = []
+
+    # If either of airports has no iata_code - calculate co2 according to formula:
+    # One CO2 gram = 1km * 90gr/km
+    for i, airport in enumerate(airports):
+        if curr.iata_code == '' or airport.iata_code == '':
+            airport.co2 = airport.distance * 90
+        else:
+    # Otherwise, make a call to API to calculate it more precisely
+            computable_flights.append({
+                'from': curr.iata_code,
+                'to': airport.iata_code
+            })
+            airport_indexes.append(i)
+
+    headers = {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': 'Bearer ' + os.environ['climatiq_key']
+    }
+
+    res = requests.post('https://beta3.api.climatiq.io/travel/flights', headers=headers, data={'legs': computable_flights})
+
+    if res.status_code == '200':
+        # Add calculated co2 to remaining airports
+        for j, flight in enumerate(res.legs):
+            airports[airport_indexes[j]].co2 = flight['co2e']
+    else:
+        print('Failed to calculate co2 emission. Error: ')
+        print(res.text)
 
 # CLASSES
 class Airport:
 
-    def __init__(self, ident, airport_name, country_name, type, lat, long):
+    def __init__(self, ident, airport_name, country_name, type, lat, long, iata_code):
         self.ident = ident
         self.airport_name = airport_name
         self.country_name = country_name
         self.type = type
         self.lat = lat
         self.long = long
+        self.iata_code = iata_code
 
 
 if __name__ == '__main__':
